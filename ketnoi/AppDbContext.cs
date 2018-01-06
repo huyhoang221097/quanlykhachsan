bcbdd1697bb2b6360aa70f285bb0395dc026de72
hoang <hoang@example.com>
2018-01-06 22:12:32 +0700

    Giao dien dang nhap

---
+++ ketnoi/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quanlykhachsan
+{
+    class DbContext
+    {
+        public static SqlConnection Cnn = null;
+        public SqlConnection _DbContext()
+        {
+            Cnn = new SqlConnection(@"Data Source=(LocalDb)\MSSQLLocalDB;;AttachDbFilename=|DataDirectory|\quanlykhachsan.mdf;Initial Catalog=quanlykhachsan;Integrated Security=True");
+            return Cnn;
+        }
+    }
+}
