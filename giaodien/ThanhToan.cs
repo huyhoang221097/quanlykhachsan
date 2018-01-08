7334925bbf179ec6c8e56a13e381770980bc7363
hoang <hoang@example.com>
2018-01-08 10:08:56 +0700

    show thong tin thanh toan

--- giaodien/ThanhToan.cs
+++ giaodien/ThanhToan.cs
@@ -16,7 +16,7 @@ namespace quanlykhachsan.giaodien
         DbContext db = new DbContext();
         private SqlCommand Cmd;
         private SqlDataAdapter da = new SqlDataAdapter();
-        string idphong,Tenkh;
+        string idphong,Tenkh,cmnd,sl,loaiphong,tg,loaitg,thanhtien;
         public ThanhToan(string x, string y)
         {
             InitializeComponent();
@@ -32,10 +32,48 @@ namespace quanlykhachsan.giaodien
             try
             {
                 Cnn.Open();
-                string tamp = "SELECT TOP(1) WITH TIES IDKH FROM KhachHang ORDER BY IDKH DESC";
+                string tamp = "SELECT TOP(1) WITH TIES TenKH FROM KhachHang k INNER JOIN ThuePhong t on k.IDKH=t.IDKH where IDPhong='" + idphong + "' ORDER BY t.ID DESC";
                 Cmd = new SqlCommand(tamp, Cnn);
                 object IdKh = Cmd.ExecuteScalar();
                 Tenkh = (string)IdKh;
+                Cnn.Close();
+
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lỗi ShowMaKh", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void ShowCMND()
+        {
+            SqlConnection Cnn = db._DbContext();
+            try
+            {
+                Cnn.Open();
+                string tamp = "SELECT TOP(1) WITH TIES CMND FROM KhachHang k INNER JOIN ThuePhong t on k.IDKH=t.IDKH where IDPhong='" + idphong + "' ORDER BY t.ID DESC";
+                Cmd = new SqlCommand(tamp, Cnn);
+                object IdKh = Cmd.ExecuteScalar();
+                cmnd = (string)IdKh;
+                Cnn.Close();
+
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lỗi ShowMaKh", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void ShowSL()
+        {
+            SqlConnection Cnn = db._DbContext();
+            try
+            {
+                Cnn.Open();
+                string tamp = "SELECT TOP(1) WITH TIES SL FROM KhachHang k INNER JOIN ThuePhong t on k.IDKH=t.IDKH where IDPhong='" + idphong + "' ORDER BY t.ID DESC";
+                Cmd = new SqlCommand(tamp, Cnn);
+                object IdKh = Cmd.ExecuteScalar();
+                cmnd = (string)IdKh;
                 Cnn.Close();
 
 
@@ -49,6 +87,9 @@ namespace quanlykhachsan.giaodien
         private void ThanhToan_Load(object sender, EventArgs e)
         {
             ShowMaKH();
+            lbTenKh.Text = Tenkh;
+            ShowCMND();
+            lbCMND.Text = cmnd;
         }
 
         private void btThanhToan_Click(object sender, EventArgs e)
